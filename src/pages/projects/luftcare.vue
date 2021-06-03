feb8a575e06bf46cd00fe4644bc02e38a4bdfaaf
Thales Agapito <thsousa101@gmail.com>
2021-06-03 20:49:16 -0300

    Add luftcare wip page

---
+++ src/pages/projects/luftcare.vue
@@ -0,0 +1,16 @@
+<template>
+  <floating-back-button />
+  <div class="flex justify-center items-center prose max-w-none min-h-screen">
+    <h2>
+      🚧 Work in progress 🚧
+    </h2>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({})
+</script>
+
+<style lang="postcss" scoped>
+</style>
